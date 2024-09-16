37bd81245d5d0bbac000c5e2ef2060f45e63a669
Toby Wilkinson <toby.wilkinson@alloyed.com>
2024-09-16 17:06:10 +0200

    Added images

--- IPMSpatialAnalysis/Properties/Resources.Designer.cs
+++ IPMSpatialAnalysis/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace IPMSpatialAnalysis.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap filter {
+            get {
+                object obj = ResourceManager.GetObject("filter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap preview {
             get {
                 object obj = ResourceManager.GetObject("preview", resourceCulture);
